651832579f78d8acf4d7c154ad4ef6e3ae9e0dbc
Ibrar Ul Hassan <f2021065212@umt.edu.pk>
2026-03-01 20:57:48 +0500

    updated url

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,6 @@ app_name = 'core'
 urlpatterns = [
     path('', views.form_view, name='form'),
     path('thank-you/', views.thank_you_view, name='thank_you'),
-    path('t/<uuid:tracking_token>/', views.track_view, name='track'),
+    path('submission/<uuid:tracking_token>/confirmation/', views.track_view, name='track'),
     path('api/track/<uuid:tracking_token>/', views.track_api_view, name='track_api'),
 ]
